e4397667b3c88405f85dc8820bce7b6b41c73d77
joshturn <talktojosh@gmail.com>
2015-09-03 18:21:21 -0700

    Add comments to problem

--- passwordCheck.js
+++ passwordCheck.js
@@ -1,9 +1,12 @@
+// Given a string containing alphanumeric characters, find the longest
+// valid password. A valid password must start with a capital letter and
+// cannot contain a number.
+
 var password = function(str){
 	var longest = 0;
 	var temp = 0;
 
 	for (var i = 0; i < str.length; i++){
-	debugger;
 	  if (str[i] === str[i].toUpperCase() && temp === 0){
 	    temp = 1;
 	  } else if (str[i] * 1 !== str[i] * 1 && temp !== 0){
@@ -15,10 +18,11 @@ var password = function(str){
 	    temp = 0;
 	  }
 	}
-
 	if (temp > longest){
 	  longest = temp;
 	}
-
 	return longest > 0 ? longest : -1;
 };
+
+// Time complexity: O(n)
+// Space complexity: O(1)
