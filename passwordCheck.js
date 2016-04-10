8659ea635f2d7d051b15e25a9ae314c9906020a3
Josh Turner <talktojosh@gmail.com>
2016-04-09 17:16:20 -0700

    Update passwordCheck.js

--- passwordCheck.js
+++ passwordCheck.js
@@ -3,25 +3,25 @@
 // cannot contain a number.
 
 var password = function(str){
-	var longest = 0;
-	var temp = 0;
+  var longest = 0;
+  var temp = 0;
 
-	for (var i = 0; i < str.length; i++){
-	  if (str[i] === str[i].toUpperCase() && temp === 0){
-	    temp = 1;
-	  } else if (str[i] * 1 !== str[i] * 1 && temp !== 0){
-	    temp++;
-	  } else {
-	    if (temp > longest){
-	      longest = temp;
-	    }
-	    temp = 0;
-	  }
-	}
-	if (temp > longest){
+  for (var i = 0; i < str.length; i++){
+    if (str[i] === str[i].toUpperCase() && temp === 0){
+      temp = 1;
+    } else if (str[i] * 1 !== str[i] * 1 && temp !== 0){
+         temp++;
+    } else {
+        if (temp > longest){
 	  longest = temp;
-	}
-	return longest > 0 ? longest : -1;
+	 }
+	 temp = 0;
+    }
+  }
+  if (temp > longest){
+    longest = temp;
+  }
+  return longest > 0 ? longest : -1;
 };
 
 // Time complexity: O(n)
