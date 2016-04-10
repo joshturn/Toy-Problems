46be72b6d3d6f7e8e5e8e0a8de8da24bf555cd7f
Josh Turner <talktojosh@gmail.com>
2016-04-09 17:03:40 -0700

    Update isomorphic strings.js

--- isomorphic strings.js
+++ isomorphic strings.js
@@ -4,31 +4,30 @@
 // No two characters may map to the same character but a character may map to itself.
 
 var iso = function(str1, str2) {
-	var firstChars = [];
-	var secondChars = [];
-	var temp = {};
-	var count = 1;
+  var firstChars = [];
+  var secondChars = [];
+  var temp = {};
+  var count = 1;
 
-	for (var i = 0; i < str1.length; i++){
+  for (var i = 0; i < str1.length; i++){
     if (!temp[str1[i]]){
-    	temp[str1[i]] = count;
-    	count++;
+      temp[str1[i]] = count;
+      count++;
     }
     firstChars.push(temp[str1[i]]);
-	}
+  }
   
   temp = {};
   count = 1;
 
-	for (var i = 0; i < str2.length; i++){
+  for (var i = 0; i < str2.length; i++){
     if (!temp[str2[i]]){
-    	temp[str2[i]] = count;
-    	count++;
+      temp[str2[i]] = count;
+      count++;
     }
     secondChars.push(temp[str2[i]]);
-	}
-
-	return firstChars.join() === secondChars.join();
+  }
+  return firstChars.join() === secondChars.join();
 };
 
 // Time complexity: O(n);
